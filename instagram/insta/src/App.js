f377db83aed554921859b903d02d1ac551a3db7b
Sean Doyle <sdoylelambda@gmail.com>
2019-04-08 18:10:49 -0400

    DummyData loading into state.

--- instagram/insta/src/App.js
+++ instagram/insta/src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import PostContainer from './components/PostContainer/PostContainer';
 import CommentSection from './components/CommentSection/CommentSection';
 import SearchBar from './components/SearchBar/SearchBar';
+import dummyData from './dummyData';
 
 class App extends Component {
+  constructor() {
+  super();
+  this.state = {
+    userData: []
+    }
+  }
+
+componentDidMount() {
+  this.setState({userData: dummyData})
+}
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            <CommentSection />
             <SearchBar />
+            <CommentSection />
             <PostContainer />
           </p>
         </header>
