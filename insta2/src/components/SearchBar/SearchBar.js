bcf30e96444d921f5c6a8da02488ebcf532c1f60
Sean Doyle <sdoylelambda@gmail.com>
2019-04-11 15:56:52 -0400

    Nav images all showing.

--- insta2/src/components/SearchBar/SearchBar.js
+++ insta2/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import insta from '../../components/images/insta.png';
+import links from '../../components/images/links.png';
 // import './SearchBar.css';
+ 
 
 function SearchBar() {
     return (
+        <div>
+        <img src={insta} alt="instagizam" />
         <p>SearchBar</p>
+        <img src={links} alt="links" />
+        </div>
     )
 }
 
